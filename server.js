8a00f8cce97461cac0dc89a707401d0e2d144c5c
MonicaMaged1 <monica.maged96@gmail.com>
2021-06-04 01:54:38 +0200

    first trial

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
+const bodyParser = require('body-parser');
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3300;
+app.listen(port, ()=> {
+    console.log("Listening on port " + port);
+})
+//GET route for projectData
+app.get('/', function (req, res) {
+    res.send(projectData);
+})
+//POST route for inserting data to projectData
+app.post('/addEntry', function (req, res) {
+    //res.send('POST received')
+    newEnty = {
+        key: req.body.key,
+        temp: req.body.temp,
+        date: req.body.date,
+        userResponse: req.body.userResponse
+    }
+    projectData.push(newEnty);
+  })
+  
+  
--- website/app.js
+++ website/app.js
@@ -1,5 +1,28 @@
 /* Global Variables */
-
+const apiKey = '8dc8c5a6749b423f3f2025a0ae311fd8';
+const baseURL = `api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}`;
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const generateBtn = document.getElementById("generate");
+
+const zipTxt = document.getElementById("zip");
+
+generateBtn.addEventListener("click", ()=>{
+    console.log('clicked');
+    if(zipTxt.value.length == 0){
+        alert('Please, enter a zip code.');
+    }
+    else{
+        zipCode = zipTxt.value;
+        console.log(baseURL);
+    }
+});
+
+
+//get weather data from API
+
+const getData = async ()=>{
+
+}
