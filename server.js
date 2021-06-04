180823e3c12893e450cd84ba3fd7f6eb7371abeb
MonicaMaged1 <monica.maged96@gmail.com>
2021-06-04 02:30:08 +0200

    Working version of getData()

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const express = require('express');
 const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
--- website/app.js
+++ website/app.js
@@ -1,6 +1,7 @@
 /* Global Variables */
 const apiKey = '8dc8c5a6749b423f3f2025a0ae311fd8';
-const baseURL = `api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}`;
+let zipCode = '';
+let baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&appid=" + apiKey;
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
@@ -16,7 +17,9 @@ generateBtn.addEventListener("click", ()=>{
     }
     else{
         zipCode = zipTxt.value;
+        baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&appid=" + apiKey;
         console.log(baseURL);
+        getData();
     }
 });
 
@@ -24,5 +27,13 @@ generateBtn.addEventListener("click", ()=>{
 //get weather data from API
 
 const getData = async ()=>{
-
+    const res = await fetch(baseURL);
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
+    catch(error) {
+        console.log("error ", error);
+    }
 }
